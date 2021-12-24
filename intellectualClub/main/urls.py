79cb3381540c3232a6b9933a4647a863976baf3e
Alyona Shabaykina <shabbaykina@mail.ru>
2021-12-24 12:56:50 +0300

    Add register user

--- intellectualClub/main/urls.py
+++ intellectualClub/main/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.EventHome.as_view(),  name="home"),
     path('about', views.about, name="about"),
-    path('regiter', views.chat, name='register'),
+    path('regiter', views.RegisterUser.as_view(), name='register'),
     path('login', views.chat, name='login'),
     path('events/<slug:event_slug>', views.ShowEventCart.as_view(), name='event'),
     path('category/<slug:cat_slug>',
--- intellectualClub/main/views.py
+++ intellectualClub/main/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core import exceptions
 from django.http.response import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls.base import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from .models import *
@@ -109,4 +110,17 @@ class AddEvent(LoginRequiredMixin, DataMixin, CreateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Добавление события')
+        return dict(list(context.items()) + list(c_def.items()))
+
+#  REGISTER USER CLASS
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'main/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация пользователей')
         return dict(list(context.items()) + list(c_def.items()))
