f527fe700621591915082fbd15d8a4916bdd7fb2
Alyona Shabaykina <shabbaykina@mail.ru>
2022-03-31 09:47:22 +0300

    Add profile picture

--- intellectualClub/account/models.py
+++ intellectualClub/account/models.py
@@ -1,3 +1,20 @@
+import profile
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField("Описание", null=True, blank=True, default=None)
+    profile_pic = models.ImageField(
+        "Изображение", null=True, upload_to='photos/profile_pic/')
+
+    def __str__(self):
+        return "%s" % self.user
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
